2b721ddcfb95839508d54ebdb2cbdbb5c16de259
MWilkinson <58124917+MRW-Code@users.noreply.github.com>
2022-05-18 15:04:44 +0100

    added ttv

--- main.py
+++ main.py
@@ -10,73 +10,106 @@ from tqdm import tqdm
 import numpy as np
 import torch.nn.functional as F
 import torch
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
-import warnings
+from dgllife.utils.splitters import SingleTaskStratifiedSplitter
+import os
+
 
 if __name__ == '__main__':
-    print(device)
+    os.makedirs('./checkpoints/models', exist_ok=True)
+
+    # Load dataset
     dataset = MultiCompSolDatasetv2()
-    dataloader = GraphDataLoader(dataset, batch_size=256, shuffle=True)
 
-    model = DoubleNet(1, 256)
+    # Split dataset into train/val/test
+    # There is also a kfold function here which might be helpful at some point
+    train_dataset, val_dataset, test_dataset = SingleTaskStratifiedSplitter.train_val_test_split(dataset, dataset.labels.reshape(-1, 1), 0)
 
+    # Create dataloaders for each split
+    train_dataloader = GraphDataLoader(train_dataset, batch_size=256, shuffle=True)
+    val_dataloader = GraphDataLoader(val_dataset, batch_size=256, shuffle=True)
+    test_dataloader = GraphDataLoader(test_dataset, batch_size=1, shuffle=True)
+
+    # Get model and stuff for training
+    model = DoubleNet(1, 1024)
     opt = torch.optim.Adam(model.parameters(), lr=1e-7)
-    epochs = 500
+    scheduler = ReduceLROnPlateau(opt, mode='min', factor=0.1, patience=5, verbose=True)
+
+    criterion = torch.nn.MSELoss()
+    epochs = 100
     model.to(device)
+    min_valid_loss = np.inf
+
+    # Training loop
     for epoch in range(epochs):
-        ls = []
-        for batch_id, batch_data in enumerate(dataloader):
+        train_loss = 0.0
+        model.train()
+        for batch_id, batch_data in enumerate(train_dataloader):
+            # Get labels and features
             batched_graphA, batched_graphB, labels = batch_data
             labels = labels.reshape(-1, 1)
-
             featsA = batched_graphA.ndata['atomic']
             featsB = batched_graphB.ndata['atomic']
 
-            logits = model(batched_graphA, featsA, batched_graphB, featsB)
-            loss = F.mse_loss(logits, labels)
-            ls.append(loss.item())
-
+            # Get preds
             opt.zero_grad()
+            logits = model(batched_graphA, featsA, batched_graphB, featsB)
+            loss = criterion(logits, labels)
+
+            # backwards pass
             loss.backward()
             opt.step()
-
-            # metrics
-            if torch.cuda.is_available():
-                labels = labels.detach().cpu().numpy()
-                logits = logits.detach().cpu().numpy()
-            else:
-                labels = labels.detach().numpy()
-                logits = logits.detach().numpy()
-
-            r2 = r2_score(labels, logits)
-            mse = mean_squared_error(labels, logits)
-            mae = mean_absolute_error(labels, logits)
-            mape = mean_absolute_percentage_error(labels, logits)
-
-        tqdm.write(f'Epoch {epoch}, Loss={np.mean(ls):.3f}, r2={r2:.3f}, mse={mse:.3f}, mae={mae:.3f}, mape={mape:.3f}')
-
-            # running_loss += loss.item()
-            # print(f'[{epoch + 1}, {epoch + 1:5d}] loss: {running_loss / 1:.3f}')
-            # running_loss = 0.0
+            train_loss += loss.item()
 
 
-    # model = GCN(in_featA=20, in_featB=20, n_hidden=20)
-    # model = GATNet(test[0].num_nodes())
-    # trainer = pl.Trainer(max_epochs=10, min_epochs=10, log_every_n_steps=2)
-    # trainer.fit(model, dataloader)
+        valid_loss = 0.0
+        model.eval()
+        for batch_id, batch_data in enumerate(val_dataloader):
+            # Get labels and features
+            batched_graphA, batched_graphB, labels = batch_data
+            labels = labels.reshape(-1, 1)
+            featsA = batched_graphA.ndata['atomic']
+            featsB = batched_graphB.ndata['atomic']
 
+            target = model(batched_graphA, featsA, batched_graphB, featsB)
+            loss = criterion(target, labels)
+            valid_loss += loss.item()
 
-    # train_featuresA, train_featuresB, train_labels = next(iter(dls))
-    # Convert the SMILES codes into molcular graphs
+        print(f'Epoch {epoch + 1} \t Training Loss: {train_loss / len(train_dataloader)} \t Validation Loss: {valid_loss / len(val_dataloader)}')
+        if min_valid_loss > valid_loss:
+            print(f'Validation Loss Decreased({min_valid_loss:.6f}--->{valid_loss:.6f}) \t Saving The Model')
+            min_valid_loss = valid_loss
+            # Saving State Dict
+            # torch.save(model.state_dict(), './checkpoints/models/best_model.pth')
+            torch.save(model, './checkpoints/models/best_model.pth')
 
+        scheduler.step(valid_loss)
 
-    # Define and create the model
+    model = torch.load('./checkpoints/models/best_model.pth')
+    model.eval()
+    all_preds = np.empty(len(test_dataloader))
+    all_labels = np.empty(len(test_dataloader))
+    for batch_id, batch_data in enumerate(test_dataloader):
+        # Get labels and features
+        batched_graphA, batched_graphB, labels = batch_data
+        labels = labels.reshape(-1, 1)
+        featsA = batched_graphA.ndata['atomic']
+        featsB = batched_graphB.ndata['atomic']
 
+        preds = model(batched_graphA, featsA, batched_graphB, featsB)
 
-    # Pytorch training loop
+        # metrics
+        if torch.cuda.is_available():
+            all_labels[batch_id] = labels.detach().cpu().numpy()
+            all_preds[batch_id] = preds.detach().cpu().numpy()
+        else:
+            all_labels[batch_id] = labels.detach().numpy()
+            all_preds[batch_id] = preds.detach().numpy()
 
-
-    # Return some metics and or figures
-
-
-    print('done')
+    r2 = r2_score(all_labels, all_preds)
+    mse = mean_squared_error(all_labels, all_preds)
+    mae = mean_absolute_error(all_labels, all_preds)
+    mape = mean_absolute_percentage_error(all_labels, all_preds)
+    print('External test set:')
+    print(f' r2={r2:.3f}, mse={mse:.3f}, mae={mae:.3f}, mape={mape:.3f}')
